ca4fbb0d88ac23a0c2551a80f524472ffd858bdf
vlambo3 <contadora.godoy.vanina@gmail.com>
2023-01-29 13:15:05 -0300

    First commit

---
+++ src/main/java/com/exercises/my/exercises/MyExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.exercises.my.exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercises/my/exercises/MyExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercises.my.exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
